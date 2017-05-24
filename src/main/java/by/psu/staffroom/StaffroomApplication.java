03ccfe581a8a47bac45ddebb35c47893989bd2d5
13itzt2abuevich <13itzt2.buevich.a@pdu.by>
2017-05-24 20:59:18 +0300

    Create the Spring Boot application

---
+++ src/main/java/by/psu/staffroom/StaffroomApplication.java
@@ -0,0 +1,23 @@
+package by.psu.staffroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * = StaffroomApplication
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@SpringBootApplication
+public class StaffroomApplication {
+
+    /**
+     * TODO Auto-generated method documentation
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(StaffroomApplication.class, args);
+    }
+}
